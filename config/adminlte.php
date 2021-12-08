1f1f5c2d60d412555e89e8086039f4ef31bb0c2b
Luis Factor <huaraz14@gmail.com>
2021-12-08 01:28:08 -0500

    Avace de Registro de Entrada y Logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,7 +46,7 @@ return [
     */
 
     'logo' => '<b>Asistencia</b>Docentes',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'vendor/adminlte/dist/img/Logo2.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
