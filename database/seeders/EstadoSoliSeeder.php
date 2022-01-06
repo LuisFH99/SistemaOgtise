66983c2db059437c0ad36c7279607b9d2378bc89
Jescenia <jesmelgarejo46@gmail.com>
2022-01-05 23:54:05 -0500

    Agregando datos

--- database/seeders/EstadoSoliSeeder.php
+++ database/seeders/EstadoSoliSeeder.php
@@ -15,11 +15,11 @@ class EstadoSoliSeeder extends Seeder
     public function run()
     {
         $esolicitud1=new EstadoSolicitud();
-        $esolicitud1-> estadoSol='Enviado';
+        $esolicitud1-> estadoSol='Enviada';
         $esolicitud1->save();
 
         $esolicitud2=new EstadoSolicitud();
-        $esolicitud2-> estadoSol='Proceso Visto Bueno Aprobado Denegado';
+        $esolicitud2-> estadoSol='Procesada';
         $esolicitud2->save();
 
         $esolicitud3=new EstadoSolicitud();
@@ -27,12 +27,16 @@ class EstadoSoliSeeder extends Seeder
         $esolicitud3->save();
 
         $esolicitud4=new EstadoSolicitud();
-        $esolicitud4-> estadoSol='Aprobado';
+        $esolicitud4-> estadoSol='Admitida';
         $esolicitud4->save();
 
         $esolicitud5=new EstadoSolicitud();
-        $esolicitud5-> estadoSol='Denegado';
+        $esolicitud5-> estadoSol='Aprobada';
         $esolicitud5->save();
+
+        $esolicitud6=new EstadoSolicitud();
+        $esolicitud6-> estadoSol='Denegada';
+        $esolicitud6->save();
 
         //
     }
