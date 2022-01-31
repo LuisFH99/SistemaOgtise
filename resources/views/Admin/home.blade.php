01b8cf19c3bb76b5482fde1b0adee628b842c569
Luis Factor <huaraz14@gmail.com>
2022-01-31 00:54:35 -0500

    cambios home

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -30,7 +30,7 @@
                     <div class="col-md-4">
                         <div class="d-flex abs-center">
                             <h1>
-                                Bienvenidos al Sistema de Asistencia Docente
+                                Bienvenidos al Sistema de Gestion de Asistencia Docente
                             </h1>
                         </div>
                     </div>
